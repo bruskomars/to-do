4bdc69719710698c8fca9c45010697adeeee302c
Mars Alexis Regidor <pythonista.mars@gmail.com>
2023-10-08 18:17:47 +0800

    todo app final

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('addTask/', views.addTask, name='addTask'),
+    path('markDone/<int:pk>', views.markDone, name='markDone'),
+    path('markUnone/<int:pk>', views.markUndone, name='markUndone'),
+    path('editTask/<int:pk>', views.editTask, name='editTask'),
+    path('deleteTask/<int:pk>', views.deleteTask, name='deleteTask'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -10,3 +10,39 @@ def addTask(request):
     )
 
     return redirect('home')
+
+def markDone(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_completed = True
+    task.save()
+
+    return redirect('home')
+
+def markUndone(request, pk):
+    task = Task.objects.get(id=pk)
+    task.is_completed = False
+    task.save()
+
+    return redirect('home')
+
+def editTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        edit_task = request.POST.get('task')
+
+        task.task = edit_task
+        task.save()
+
+        return redirect('home')
+
+    context = {
+        'task':task
+    }
+    return render(request, 'edit.html', context)
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return redirect('home')
