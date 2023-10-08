4453afef35116f11f41531d340f695abcf561af2
Mars Alexis Regidor <pythonista.mars@gmail.com>
2023-10-08 16:40:33 +0800

    add task function

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from todos.models import Task
 
 def home(request):
-    return render(request, 'home-todo.html')
+    uncompleted_tasks = Task.objects.filter(is_completed=False).order_by('-created_at')
+    completed_tasks = Task.objects.filter(is_completed=True)
+
+    context = {
+        'uncompleted_tasks': uncompleted_tasks,
+        'completed_tasks': completed_tasks,
+    }
+
+    return render(request, 'home-todo.html', context)
